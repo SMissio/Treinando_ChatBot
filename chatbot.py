5f0fb8f90b278a2cc5b5ec8918d41f600a907e82
Sylvia Missio <59312305+SMissio@users.noreply.github.com>
2022-01-24 17:15:00 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -56,3 +56,86 @@ def limpar_texto(texto):
     return texto
 
 limpar_texto("ExeMplo i'm #@")
+
+perguntas_limpas = []
+for pergunta in perguntas:
+    perguntas_limpas.append(limpar_texto(pergunta))
+
+respostas_limpas = []
+for resposta in respostas:
+    respostas_limpas.append(limpar_texto(resposta))
+
+palavras_contagem = {}
+for pergunta in perguntas_limpas:
+    for palavra in pergunta.split():
+        if palavra not in palavras_contagem:
+            palavras_contagem[palavra] = 1
+        else:
+            palavras_contagem[palavra] +=1
+
+for resposta in respostas_limpas:
+    for palavra in resposta.split():
+        if palavra not in palavras_contagem:
+            palavras_contagem[palavra] = 1
+        else:
+            palavras_contagem[palavra] +=1
+
+#Palavras nao frequentes
+            
+limite = 20
+perguntas_palavras_int = {}
+numero_palavra = 0
+for palavra, contagem in palavras_contagem.items():
+    if contagem >= limite:
+        perguntas_palavras_int[palavra] = numero_palavra
+        numero_palavra += 1
+
+respostas_palavras_int = {}
+numero_palavra = 0
+for palavra, contagem in palavras_contagem.items():
+    if contagem >= limite:
+        respostas_palavras_int[palavra] = numero_palavra
+        numero_palavra += 1
+
+tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
+for token in tokens:
+    perguntas_palavras_int[token] = len(perguntas_palavras_int) + 1
+for token in tokens:
+    respostas_palavras_int[token] = len(respostas_palavras_int) + 1
+    
+#invervsao da chave do dicionario
+# p_i = palavras (variavel) utilizando for na sequencia
+respostas_int_palavras = {p_i: p for p, p_i in respostas_palavras_int.items()}
+#token
+for i in range(len(respostas_limpas)):
+    respostas_limpas[i] += ' <EOS>'
+#----trad inteiros e palavras menos frequentes
+perguntas_para_int = []
+for pergunta in perguntas_limpas:
+    ints = []
+    for palavra in pergunta.split():
+        if palavra not in perguntas_palavras_int:
+            ints.append(perguntas_palavras_int['<OUT>'])
+        else:
+            ints.append(perguntas_palavras_int[palavra])
+    perguntas_para_int.append(ints)
+
+respostas_para_int = []
+for resposta in respostas_limpas:
+    ints = []
+    for palavra in resposta.split():
+        if palavra not in respostas_palavras_int:
+            ints.append(respostas_palavras_int['<OUT>'])
+        else:
+            ints.append(respostas_palavras_int[palavra])
+    respostas_para_int.append(ints)
+
+#ordem
+perguntas_limpas_ordenadas = []
+respostas_limpas_ordenadas = []
+for tamanho in range (1, 25 + 1):
+    for i in enumerate(perguntas_para_int):
+        if len(i[1]) == tamanho:
+            perguntas_limpas_ordenadas.append(perguntas_para_int[i[0]])
+            respostas_limpas_ordenadas.append(respostas_para_int[i[0]])
+#Seq2Seq construção:
