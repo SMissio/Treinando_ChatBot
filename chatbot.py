31319d7cca6c758927765f31987837f81a7aec4f
Sylvia Missio <59312305+SMissio@users.noreply.github.com>
2022-01-24 14:27:23 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -24,3 +24,35 @@ for conversa in conversas[:-1]:
     _conversa = conversa.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
     #print(_conversa)
     conversas_id.append(_conversa.split(','))
+
+
+perguntas = []
+respostas = []
+
+for conversa in conversas_id:
+    #print(conversa)
+    #print(********)
+    for i in range (len(conversa) - 1):
+        #print(i)
+        perguntas.append(id_linha[conversa[i]])
+        respostas.append(id_linha[conversa[i+1]])
+
+def limpar_texto(texto):
+    texto = texto.lower()
+    texto = re.sub(r"i'm","i am",texto)
+    texto = re.sub(r"he's", "he is", texto)
+    texto = re.sub(r"she's", "she is", texto)
+    texto = re.sub(r"that's", "that is", texto)
+    texto = re.sub(r"what's", "what is", texto)
+    texto = re.sub(r"where's", "Where is", texto)
+    texto = re.sub(r"\'ll", " will", texto)
+    texto = re.sub(r"\'ve", " have", texto)
+    texto = re.sub(r"\'re", " are", texto)
+    texto = re.sub(r"\'d", " would", texto)
+    texto = re.sub(r"won't", "will not", texto)
+    texto = re.sub(r"can't", "cannot", texto)
+    texto = re.sub(r"[-()#/@;:<>{}~+=?.|,]", "", texto)
+    
+    return texto
+
+limpar_texto("ExeMplo i'm #@")
